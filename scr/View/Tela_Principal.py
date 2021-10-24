94c1863ea32a2a9cd6f0fe2b2264da0414b05363
Marcelo Cardoso <marcelosouza7r@gmail.com>
2021-10-23 23:41:03 -0300

    Selecionar planilha pelo explorador de arquivos do OS

--- scr/View/Tela_Principal.py
+++ scr/View/Tela_Principal.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from functools import partial
+from tkinter.filedialog import askopenfilename
 
 from scr.View import Tela_Adicionar as ta
 from scr.View import Tela_Excluir as te
@@ -24,29 +25,44 @@ def Tela_Principal():
     janela.minsize(800, 300)
     janela.title("Manipulador Excel")
 
-    texto_informar_caminho = Label(janela,
-    text="Informe o caminho junto ao nome do arquivo.xlsx e aperte ENTER: ", fg='#808080')
-    texto_informar_caminho.place(relx=0.10, rely=0.10)
-    texto_informar_caminho.config(font=("Ubuntu", 14))
+    texto_informar = Label(janela,
+    text="Selecione a planilha e inicie as operações: ", fg='#808080')
+    texto_informar.place(relx=0.10, rely=0.10)
+    texto_informar.config(font=("Ubuntu", 14))
 
-    entrada = Entry(janela, font=("Ubuntu", 14))
-    entrada.place(relx=0.1, rely=0.25, relwidth=0.8, relheight=0.16)
+    aviso = Label(janela,
+    text="formatos aceitos: .xlsx, .xlsm, .xltx, .xltm ", fg='#808080')
+    aviso.place(relx=0.10, rely=0.2)
+    aviso.config(font=("Ubuntu", 12))
+
+    caminho = []
+
+    Selecionar_Planilha = Button(janela,
+                               text="Selecionar planilha",
+                               command=lambda: caminho.append(askopenfilename(filetypes=([("Planilhas", "*.xlsx; *.xlsm *.xltx; *.xltm")]), title='Selecionar planilha')),
+                               activebackground='#345',
+                               activeforeground='white'
+    )
+    Selecionar_Planilha.place(relx=0.10, rely=0.40)
 
     adicionar = Button(janela,
                        text="Adicionar Item",
-                       command=entrada.bind("<Return>",
-                       (lambda event: PegarCaminhoAdd(adicionar, entrada.get()))),
+                       command=lambda: PegarCaminhoAdd(adicionar, caminho[0]),
                        activebackground='#345',
                        activeforeground='white'
     )
-    adicionar.place(relx=0.10, rely=0.60)
+    adicionar.place(relx=0.10, rely=0.65)
 
     remover = Button(janela, text="Excluir Item",
-                     command=entrada.bind("<Enter>",
-                     (lambda event: PegarCaminhoRemo(remover, entrada.get()))),
+                     command=lambda: PegarCaminhoRemo(remover, caminho[0]),
                      activebackground='#345',
                      activeforeground='white'
     )
-    remover.place(relx=0.28, rely=0.60)
+    remover.place(relx=0.26, rely=0.65)
 
     janela.mainloop()
+
+
+Tela_Principal()
+
+
